0cf79b7ed676e4b2c1c7cb60b6dc3a38016c1ad0
Chris Jones <cjfj@google.com>
2023-06-26 11:01:35 -0700

    Add `get_serialized_metadata` function to retrieve metadata from op's opaque data.
    PiperOrigin-RevId: 543486413

--- jaxlib/gpu/triton.cc
+++ jaxlib/gpu/triton.cc
@@ -10,7 +10,6 @@
 #include <variant>
 #include <vector>
 
-#include "pybind11/buffer_info.h"
 #include "pybind11/pybind11.h"
 #include "pybind11/pytypes.h"
 #include "pybind11/stl.h"
@@ -539,6 +538,27 @@ class AutotunedKernelCall : public KernelCallBase {
   absl::Status autotune_status_;
 };
 
+absl::StatusOr<std::string> ZlibUncompress(absl::string_view compressed) {
+  std::string data;
+  uLongf dest_len = 5 * compressed.size();
+  while (true) {
+    data.resize(dest_len);
+    int ret = uncompress(reinterpret_cast<Bytef*>(data.data()), &dest_len,
+                         reinterpret_cast<const Bytef*>(compressed.data()),
+                         compressed.size());
+    if (ret == Z_OK) {
+      // `uncompress` overwrites `dest_len` with the uncompressed size.
+      data.resize(dest_len);
+      break;
+    } else if (ret == Z_BUF_ERROR) {
+      dest_len *= 2;  // The string buffer wasn't large enough.
+    } else {
+      return absl::InvalidArgumentError("Failed to uncompress opaque data.");
+    }
+  }
+  return data;
+}
+
 absl::StatusOr<KernelCallBase*> GetKernelCall(absl::string_view opaque) {
   static absl::Mutex mutex;
   static auto& kernel_calls =
@@ -550,23 +570,7 @@ absl::StatusOr<KernelCallBase*> GetKernelCall(absl::string_view opaque) {
   if (ABSL_PREDICT_TRUE(it != kernel_calls.end())) return it->second.get();
 
   // The opaque data is a zlib compressed protobuf.
-  std::string serialized;
-  uLongf dest_len = 5 * opaque.size();
-  while (true) {
-    serialized.resize(dest_len);
-    int ret = uncompress(reinterpret_cast<Bytef*>(serialized.data()), &dest_len,
-                         reinterpret_cast<const Bytef*>(opaque.data()),
-                         opaque.size());
-    if (ret == Z_OK) {
-      // `uncompress` overwrites `dest_len` with the uncompressed size.
-      serialized.resize(dest_len);
-      break;
-    } else if (ret == Z_BUF_ERROR) {
-      dest_len *= 2;  // The string buffer wasn't large enough.
-    } else {
-      return absl::InvalidArgumentError("Failed to uncompress opaque data.");
-    }
-  }
+  JAX_ASSIGN_OR_RETURN(std::string serialized, ZlibUncompress(opaque));
 
   TritonAnyKernelCall proto;
   if (!proto.ParseFromString(serialized)) {
@@ -652,11 +656,10 @@ PYBIND11_MODULE(_triton, m) {
                     std::vector<KernelCall::Parameter>>())
       .def("to_proto",
            [](const KernelCall& kernel_call,
-              py::bytes metadata) -> absl::StatusOr<py::bytes> {
+              absl::string_view metadata) -> absl::StatusOr<py::bytes> {
              TritonAnyKernelCall proto;
              *proto.mutable_kernel_call() = kernel_call.ToProto();
-             py::buffer_info metadata_info(py::buffer(metadata).request());
-             proto.set_metadata(metadata_info.ptr, metadata_info.size);
+             proto.set_metadata(metadata);
              std::string serialized = proto.SerializeAsString();
              return py::bytes(serialized.data(), serialized.size());
            });
@@ -678,11 +681,10 @@ PYBIND11_MODULE(_triton, m) {
       }))
       .def("to_proto",
            [](const AutotunedKernelCall& kernel_call,
-              py::bytes metadata) -> absl::StatusOr<py::bytes> {
+              absl::string_view metadata) -> absl::StatusOr<py::bytes> {
              TritonAnyKernelCall proto;
              *proto.mutable_autotuned_kernel_call() = kernel_call.ToProto();
-             py::buffer_info metadata_info(py::buffer(metadata).request());
-             proto.set_metadata(metadata_info.ptr, metadata_info.size);
+             proto.set_metadata(metadata);
              std::string serialized = proto.SerializeAsString();
              return py::bytes(serialized.data(), serialized.size());
            });
@@ -699,6 +701,17 @@ PYBIND11_MODULE(_triton, m) {
         &minor, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR, device));
     return major * 10 + minor;
   });
+
+  m.def(
+      "get_serialized_metadata",
+      [](absl::string_view opaque) -> absl::StatusOr<py::bytes> {
+        JAX_ASSIGN_OR_RETURN(std::string serialized, ZlibUncompress(opaque));
+        TritonAnyKernelCall proto;
+        if (!proto.ParseFromString(serialized)) {
+          return absl::InvalidArgumentError("Failed to parse serialized data.");
+        }
+        return py::bytes(proto.metadata().data(), proto.metadata().size());
+      });
 }
 
 }  // namespace jax_triton
